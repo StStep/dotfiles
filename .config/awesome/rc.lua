369110d21836b4e914a08e1c5ed791e21ccc01d4
R. Stephens Summerlin <Robert.Summerlin@gtri.gatech.edu>
2016-08-05 13:36:12 -0400

    Added screenlock shortcut to awesome

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -221,6 +221,7 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "Left",   awful.tag.viewprev       ),
     awful.key({ modkey,           }, "Right",  awful.tag.viewnext       ),
     awful.key({ modkey,           }, "Escape", awful.tag.history.restore),
+    awful.key({ modkey, "Control" }, "l", function () awful.util.spawn("gnome-screensaver-command -l") end),
 
     awful.key({ modkey,           }, "j",
         function ()
