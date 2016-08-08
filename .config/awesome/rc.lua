fc7cf64c0999875db96e56b6525dc295eecf383a
R. Stephens Summerlin <Robert.Summerlin@gtri.gatech.edu>
2016-08-08 16:39:22 -0400

    Added awesome rainbow theme

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -41,7 +41,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init("/usr/share/awesome/themes/zenburn/theme.lua")
+beautiful.init("/home/rsummerlin6/.config/awesome/themes/rainbow/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "x-terminal-emulator"
